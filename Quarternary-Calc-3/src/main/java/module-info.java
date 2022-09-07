1db02fd4976077d98d2a246a4f4df2788842eeec
Joshua Neumann <janeumann@bsu.edu>
2022-09-07 15:13:22 -0400

    Cleaned up some unnecessary files within the project

--- Quarternary-Calc-3/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.quarternarycalc3 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.kordamp.bootstrapfx.core;
-
-    opens com.example.quarternarycalc3 to javafx.fxml;
-    exports com.example.quarternarycalc3;
-}
