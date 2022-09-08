4a1d7e35936ed5d2c3fe20928d46f9ad8f4f2216
Joshua Neumann <janeumann@bsu.edu>
2022-09-08 19:33:04 -0400

    Added Exception tests for the all previously added files. Most are not functioning at the moment. Also edited division with try/catch operator that could reworked.

--- src/main/java/com/example/quarternarycalc3/Calculator/Calculator.java
+++ src/main/java/com/example/quarternarycalc3/Calculator/Calculator.java
@@ -20,6 +20,14 @@ public class Calculator {
     }
 
     public int division(int num1, int num2){
+        try{
+            int number1 = 10;
+            int number2 = 0;
+            int result = number1/number2;
+            System.out.println(result);
+        } catch(Exception e){
+            return 0;
+        }
         return num1 / num2;
     }
 
