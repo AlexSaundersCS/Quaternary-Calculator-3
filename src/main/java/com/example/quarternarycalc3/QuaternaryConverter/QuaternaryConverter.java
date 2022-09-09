80c24f0f9501fb170a61eed5dad88abb96b8c5a2
AlexSaundersCS <alexdsaunders@gmail.com>
2022-09-09 13:12:17 -0400

    Adjusted toDecimal function and tests to use arrays

--- src/main/java/com/example/quarternarycalc3/QuaternaryConverter/QuaternaryConverter.java
+++ src/main/java/com/example/quarternarycalc3/QuaternaryConverter/QuaternaryConverter.java
@@ -1,16 +1,14 @@
 package com.example.quarternarycalc3.QuaternaryConverter;
-
 public class QuaternaryConverter {
     public QuaternaryConverter() {
 
     }
-    public int toDecimal(String quaternaryNumber){
-        char[] digitsList = quaternaryNumber.toCharArray(); //list of digits in input string
-        int quaternaryPower = quaternaryNumber.length() - 1;
+    public int toDecimal(int[] quaternaryNumber){
+        int quaternaryPower = quaternaryNumber.length - 1;
         int decimalNumber = 0;
 
-        for (char digit : digitsList) {
-            decimalNumber += Character.getNumericValue(digit) * (4 ^ quaternaryPower);
+        for (int digit : quaternaryNumber) {
+            decimalNumber += digit * (4 ^ quaternaryPower);
             quaternaryPower -= 1;
         }
 
